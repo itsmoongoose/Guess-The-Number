a1919b4dab2bbd8e6654f7fc834440896dcf8390
Lewis Lemay <lewishlemay@gmail.com>
2025-08-02 17:03:43 -0600

    Fix event listener syntax not allowing program to run

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,61 +5,55 @@ let menuClicked;
 //Function -- main
 async function main() {
 
-    //Makes sure all content is loaded before executing code
-    document.addEventListener('DOMContentLoaded', function() {
+    //Defining Variables
+    const menubtn = document.getElementById("menubtn");
+    const playbtn = document.getElementById("playbtn");
+    const leaderboardbtn = document.getElementById("leaderboardbtn");
+    const arcadeLogo = document.getElementById("arcade-logo");
 
-        //Defining Variables
-        const menubtn = document.getElementById("menubtn");
-        const playbtn = document.getElementById("playbtn");
-        const leaderboardbtn = document.getElementById("leaderboardbtn");
-        const arcadeLogo = document.getElementById("arcade-logo");
+    //Game start
+    menubtn.style.display = 'none';
 
-        //Game start
-        menubtn.style.display = 'none';
+    //Menu -- play button
+    playbtn.addEventListener('click', function() {
 
-            //Menu -- play button
-            playbtn.addEventListener('click', () => {
+        //Hiding arcade logo and buttons
+        arcadeLogo.style.visibility = "hidden";
+        playbtn.style.display = 'none';
+        leaderboardbtn.style.display = 'none';
+        menubtn.style.display = 'inline-block';
 
-                //Hiding arcade logo and buttons
-                arcadeLogo.style.visibility = "hidden";
-                playbtn.style.display = 'none';
-                leaderboardbtn.style.display = 'none';
-                menubtn.style.display = 'inline-block';
+    //Game starts
+        
+    });
 
-            //Game starts
-                
-            });
+    //Menu -- leaderboard button
+    leaderboardbtn.addEventListener('click', function() {
 
-            //Menu -- leaderboard button
-            leaderboardbtn.addEventListener('click', () => {
+        //Hiding arcade logo and buttons
+        arcadeLogo.style.visibility = "hidden";
+        playbtn.style.display = 'none';
+        leaderboardbtn.style.display = 'none';
+        menubtn.style.display = 'inline-block';
 
-                //Hiding arcade logo and buttons
-                arcadeLogo.style.visibility = "hidden";
-                playbtn.style.display = 'none';
-                leaderboardbtn.style.display = 'none';
-                menubtn.style.display = 'inline-block';
+        //Displaying scores
+        for (let score of leaderboardArray) {
+            output(score);
+        }
+    });
 
-                //Displaying scores
-                for (let score of leaderboardArray) {
-                    output(score);
-                }
-            });
+    //Menu -- return to menu
+    menubtn.addEventListener('click', function() {
+        menubtn.style.display = 'none'; //hide menu button
+        menuClicked = true;
 
-            //Menu -- return to menu
-            menubtn.addEventListener('click', () => {
-                menubtn.style.display = 'none'; //hide menu button
-                menuClicked = true;
-
-                //Display Menu -- reveal play and leaderboard buttons and arcade logo
-                document.getElementById("menubtn").onclick = function() {
-                    document.getElementById("arcade-logo").style.visibility = "visible";
-                }
-                playbtn.style.display = 'inline-block';
-                leaderboardbtn.style.display = 'inline-block';
-            });
-
-        //DOMContentLoaded event listener ends
-        });
+        //Display Menu -- reveal play and leaderboard buttons and arcade logo
+        document.getElementById("menubtn").onclick = function() {
+            document.getElementById("arcade-logo").style.visibility = "visible";
+        }
+        playbtn.style.display = 'inline-block';
+        leaderboardbtn.style.display = 'inline-block';
+    });
 
 //main function ends    
 }
