45400bfffd2243fd179691ffd1aed831833a9da4
itsmoongoose <lewishlemay@gmail.com>
2025-08-05 20:16:51 -0600

    Remove try catch statement and update throw statement to regular output

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -41,131 +41,126 @@ function menuMessage() {
 //Function -- main
 async function main() {
 
-    try {
-        //Hiding elements
+    //Hiding elements
+    menubtn.style.display = 'none';
+    container.style.dispaly = 'none';
+    containerOutput.style.display = 'none';
+
+    //Menu -- play button
+    playbtn.addEventListener('click', async function() {
+
+        //Hiding and revealing elements
+        hideElements();
+
+        //Game -- generate number
+        let correctNum = Math.round(Math.random() * 99) + 1;
+        output(correctNum);
+        let guessTracker = 0;
+        output("Guess a number between 1 and 100: ");
+        let userGuess = await input("");
+
+        //Incorrect Guess
+        while (userGuess != correctNum) {
+
+            //Invalid Guess -- throws error
+            while (isNaN(userGuess) || !userGuess || userGuess < 1 || userGuess > 100) {
+                output("ERROR - Guess must be a number between 1 and 100: ");
+                userGuess = await input("");
+            }
+
+            //Guess is too high
+            if (userGuess > correctNum) {
+                guessTracker = guessTracker + 1;
+                output("Too High! Guess Again: ");
+                userGuess = await input("");
+            }
+
+            //Guess is too low
+            if (userGuess < correctNum) {
+                guessTracker = guessTracker + 1;
+                output("Too Low! Guess Again: ");
+                userGuess = await input("");
+            }
+        }
+
+        //Correct Guess
+        if (userGuess == correctNum) {
+            guessTracker = guessTracker + 1;
+            let leaderboardName = "";
+
+            //1 guess
+            if (guessTracker == 1) {
+                output("INCREDIBLE! You guessed the number in a single try! \nPlease enter your name to be displayed on the leaderboard: ");
+                leaderboardName = await input("");
+                leaderboardName = leaderboardName.trim();
+                leaderboard1[leaderboardName] = guessTracker; //adding name and score to leaderboard1
+            } else {
+
+                //Multiple guesses
+                output("Congratulations! You guessed the number in " + guessTracker + " tries! \nPlease enter your name to be displayed on the leaderboard: ");
+                leaderboardName = await input("");
+                leaderboardName = leaderboardName.trim();
+                leaderboard2[leaderboardName] = guessTracker; //adding name and score to leaderboard2
+            }
+
+            //Invalid leaderboard name
+            while (!leaderboardName || leaderboardName in leaderboard1 || leaderboardName in leaderboard2) {
+                
+                //empty name
+                if (!leaderboardName) {
+                    output("You must enter at least 1 non-whitespace character to continue: ");
+                    leaderboardName = await input("");
+                    leaderboardName = leaderboardName.trim();
+
+                //name already exists in leaderboard
+                } else if (leaderboardName in leaderboard1 || leaderboardName in leaderboard2) {
+                    output("This name is already on the leaderboard. Please choose another: ");
+                    leaderboardName = await input("");
+                    leaderboardName = leaderboardName.trim();
+                }
+            }
+
+            //adding name to leaderboard
+            if (guessTracker == 1) {
+                leaderboard1[leaderboardName] = guessTracker;
+            } else {
+                leaderboard2[leaderboardName] = guessTracker;
+            }
+
+            //return to menu message
+            menuMessage();
+        }
+    });
+
+    //Menu -- leaderboard button
+    leaderboardbtn.addEventListener('click', function() {
+
+        //Hiding and revealing elements
+        hideElements();
+
+        //Displaying scores
+        for (let [username, score] of Object.entries(leaderboard1)) {
+            output(`${username}  ${score}`);
+        }
+        for (let [username, score] of Object.entries(leaderboard2)) {
+            output(`${username}  ${score}`);
+        }
+    });
+
+    //Menu -- return to menu
+    menubtn.addEventListener('click', function() {
+
+        //Display Menu -- hiding and revealing elements
         menubtn.style.display = 'none';
-        container.style.dispaly = 'none';
+        arcadeLogo.style.display = 'inline-block';
+        playbtn.style.display = 'inline-block';
+        leaderboardbtn.style.display = 'inline-block';
+        container.style.display = 'none';
         containerOutput.style.display = 'none';
 
-        //Menu -- play button
-        playbtn.addEventListener('click', async function() {
-
-            //Hiding and revealing elements
-            hideElements();
-
-            //Game -- generate number
-            let correctNum = Math.round(Math.random() * 99) + 1;
-            output(correctNum);
-            let guessTracker = 0;
-            output("Guess a number between 1 and 100: ");
-            let userGuess = await input("");
-
-            //Incorrect Guess
-            while (userGuess != correctNum) {
-
-                //Invalid Guess -- throws error
-                while (isNaN(userGuess) || !userGuess || userGuess < 1 || userGuess > 100) {
-                    throw new Error("Guess must be a number between 1 and 100");
-                }
-                
-                //Guess is too high
-                if (userGuess > correctNum) {
-                    guessTracker = guessTracker + 1;
-                    output("Too High! Guess Again: ");
-                    userGuess = await input("");
-                }
-
-                //Guess is too low
-                if (userGuess < correctNum) {
-                    guessTracker = guessTracker + 1;
-                    output("Too Low! Guess Again: ");
-                    userGuess = await input("");
-                }
-            }
-
-            //Correct Guess
-            if (userGuess == correctNum) {
-                guessTracker = guessTracker + 1;
-                let leaderboardName = "";
-
-                //1 guess
-                if (guessTracker == 1) {
-                    output("INCREDIBLE! You guessed the number in a single try! \nPlease enter your name to be displayed on the leaderboard: ");
-                    leaderboardName = await input("");
-                    leaderboardName = leaderboardName.trim();
-                    leaderboard1[leaderboardName] = guessTracker; //adding name and score to leaderboard1
-                } else {
-
-                    //Multiple guesses
-                    output("Congratulations! You guess the number in " + guessTracker + " tries! \nPlease enter your name to be displayed on the leaderboard: ");
-                    leaderboardName = await input("");
-                    leaderboardName = leaderboardName.trim();
-                    leaderboard2[leaderboardName] = guessTracker; //adding name and score to leaderboard2
-                }
-
-                //Invalid leaderboard name
-                while (!leaderboardName || leaderboardName in leaderboard1 || leaderboardName in leaderboard2) {
-                    
-                    //empty name
-                    if (!leaderboardName) {
-                        output("You must enter at least 1 non-whitespace character to continue: ");
-                        leaderboardName = await input("");
-                        leaderboardName = leaderboardName.trim();
-
-                    //name already exists in leaderboard
-                    } else if (leaderboardName in leaderboard1 || leaderboardName in leaderboard2) {
-                        output("This name is already on the leaderboard. Please choose another: ");
-                        leaderboardName = await input("");
-                        leaderboardName = leaderboardName.trim();
-                    }
-                }
-
-                //adding name to leaderboard
-                if (guessTracker == 1) {
-                    leaderboard1[leaderboardName] = guessTracker;
-                } else {
-                    leaderboard2[leaderboardName] = guessTracker;
-                }
-
-                //return to menu message
-                menuMessage();
-            }
-        });
-
-        //Menu -- leaderboard button
-        leaderboardbtn.addEventListener('click', function() {
-
-            //Hiding and revealing elements
-            hideElements();
-
-            //Displaying scores
-            for (let [username, score] of Object.entries(leaderboard1)) {
-                output(`${username}  ${score}`);
-            }
-            for (let [username, score] of Object.entries(leaderboard2)) {
-                output(`${username}  ${score}`);
-            }
-        });
-
-        //Menu -- return to menu
-        menubtn.addEventListener('click', function() {
-
-            //Display Menu -- hiding and revealing elements
-            menubtn.style.display = 'none';
-            arcadeLogo.style.display = 'inline-block';
-            playbtn.style.display = 'inline-block';
-            leaderboardbtn.style.display = 'inline-block';
-            container.style.display = 'none';
-            containerOutput.style.display = 'none';
-
-            //Removing previous outputs
-            containerOutput.innerHTML = "";
-        });
-
-    //try statement ends
-    } catch(error) {
-        output(`Error: ${error}`);
-    }
+        //Removing previous outputs
+        containerOutput.innerHTML = "";
+    });
 
 //main function ends
 }
